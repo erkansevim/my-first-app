5d6d11b8d5b3c47658dc0acd8333092b750cb9db
erkansevim <erkansevim.essgmail.com>
2024-07-03 11:22:55 +0200

    externel css import

--- src/msg/Msg.jsx
+++ src/msg/Msg.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import resim from "../assests/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg";
+import "./msg.css"
+
+//internal css
+const styleA={
+  color:"green",
+  fontFamily:"tahoma"
+}
 
 const Msg = () => {
   return (
     <div>
       <p style={{ color: "red" }}>Erkan Sevim</p>
+      <p style={styleA}>Erkan Sevim</p>
       <h2>Burasi Msg alani</h2>
       <p>hosgeldin react</p>
-      <img src={resim} alt="" />
+      <img className="navbarPhoto" src={resim} alt="" />
+
     </div>
   );
 };
